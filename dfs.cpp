c712081d7642421d03eab4a2e2e4cc4f7dbc4017
Sklyarov Mikhail <ategro@yandex.ru>
2016-12-13 03:32:42 +0700

    issue #2 реализован алгоритм
    Реализована основная часть алгоритма обхода в глубину

---
+++ dfs.cpp
@@ -0,0 +1,27 @@
+#ifndef NSU_DFS
+#define NSU_DFS
+
+#incldue <vector>
+
+// color - цвет вершины (0, 1, 2)
+// time_in - время захода в вершину
+// time_out - время выхода из вершины
+// start_time - начальное время
+void dfs(const vector<vector<int>> & graph, const int start, 
+		vector<bool> & color, vector<int> & time_in, vector<int> & time_out,
+		int & start_time = 0) {
+	time_in[start] = dfs_timer++;
+	color[start] = 1;
+	
+	for (size_t i = 0; i < graph[start].size(); ++i) {
+		int to = graph[start][i];
+		if (color[to] == 0) {
+			dfs(graph, to, color, time_in, time_out, start_time);
+		}
+	}
+	
+	time_out[start] = dfs_timer++;
+	color[start] = 2;	
+}
+
+#endif
