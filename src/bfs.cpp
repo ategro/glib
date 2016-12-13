1f64ef7abc7d992ea2271e811b6caf15ce12089c
Sklyarov Mikhail <ategro@yandex.ru>
2016-12-13 23:04:42 +0700

    issue #1 реструктурирование ветки
    Сделано слияние с веткой мастера. Переписан алгоритм. Добавлены тесты.
    Добавлена документация.

---
+++ src/bfs.cpp
@@ -0,0 +1,25 @@
+#include "bfs.hpp"
+#include <queue>
+
+using std::vector;
+
+void bfs(const vector<vector<int>> & graph, const int start,
+		vector<bool> & used, vector<int> & distance, vector<int> & parent) {
+    queue<int> q;
+	q.push(start);
+	used[start] = true;	
+
+	while (!q.empty()) {
+		int v = q.front();
+		q.pop();
+		for (size_t i = 0; i < graph[v].size(); ++i) {
+			int to = graph[v][i];
+			if (!used[to]) {
+				q.push(to);
+				used[to] = true;
+				distance[to] = distance[v] + 1;
+				parent[to] = v;
+			}
+		}
+	}
+}
---
+++ tests/utest_bfs.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include "bfs.hpp"
+
+using std::vector;
+
+TEST(BFStest, simple) {
+	vector<vector<int>> graph(3);
+	graph[1].push_back(2);
+	int start = 0;
+	vector<bool> used(3);
+	vector<int> distance(3);
+	vector<int> parent(3);
+
+	bfs(graph, start, used, distance, parent);
+	
+	EXPECT_EQ(true, used[0]);
+	EXPECT_EQ(true, used[1]);
+	EXPECT_EQ(false, used[2]);
+	EXPECT_EQ(0, distance[0]);
+	EXPECT_EQ(1, distance[1]);
+	EXPECT_EQ(0, distance[2]);
+	EXPECT_EQ(0, parent[0]);
+	EXPECT_EQ(0, parent[1]);
+	EXPECT_EQ(0, parent[2]);
+}
