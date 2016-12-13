877e1e27ce4a4659f5803876fa335e96ea061b66
Sklyarov Mikhail <ategro@yandex.ru>
2016-12-14 00:56:15 +0700

    issue #2 ошибка с параметром по умолчанию
    Ошибка с параметром по умолчанию

--- src/dfs.cpp
+++ src/dfs.cpp
@@ -4,11 +4,11 @@ using std::vector;
 
 int dfs(const vector<vector<int>> & graph, const int start, 
 		vector<bool> & color, vector<int> & time_in, vector<int> & time_out,
-		int start_time = 0) {
+		int start_time) {
 	time_in[start] = start_time++;
 	color[start] = 1;
 	
-	for (size_t i = 0; i < graph[start].size(); ++i) {
+	for (int i = 0; i < graph[start].size(); ++i) {
 		int to = graph[start][i];
 		if (color[to] == 0) {
 			start_time = dfs(graph, to, color, time_in, time_out, start_time);
