b8bd525bc61c861a3634a8485d2b8a04ac754dd0
Sklyarov Mikhail <ategro@yandex.ru>
2016-12-14 02:14:49 +0700

    issue #3 update tests
    Update tests

--- tests/utest_dijkstra.cpp
+++ tests/utest_dijkstra.cpp
@@ -1,7 +1,11 @@
 #include <gtest/gtest.h>
+#include "dijkstra.hpp"
  
 using std::vector;
 
+TEST(Dijkstratest, simple) {
+	vector<vector<pair<int, int>>> graph(3);
+	graph[0].push_back({1, 5});
 	int start = 0;
 	vector<bool> used(3);
 	vector<int> distance(3);
@@ -12,6 +16,9 @@ using std::vector;
 	EXPECT_EQ(true, used[0]);
 	EXPECT_EQ(true, used[1]);
 	EXPECT_EQ(false, used[2]);
+	EXPECT_EQ(0,          distance[0]);
+	EXPECT_EQ(5,          distance[1]);
+	EXPECT_EQ(1000000000, distance[2]);
 	EXPECT_EQ(0, parent[0]);
 	EXPECT_EQ(0, parent[1]);
 	EXPECT_EQ(0, parent[2]);
